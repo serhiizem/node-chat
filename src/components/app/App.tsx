db9a0e2298a599b84dd9210b217dc7c43d25c406
Serhii Zemlianyi <serhiizem@gmail.com>
2024-01-19 23:34:04 +0200

    setup project structure

---
+++ src/components/app/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Box, TextField} from "@mui/material";
+
+export const App = () => {
+    return (
+        <Box>
+            <TextField label="Required"/>
+        </Box>
+    );
+}
