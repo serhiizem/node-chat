e994cd8d9a4c0496bd839fde24313c3037caac21
Serhii Zemlianyi <serhiizem@gmail.com>
2024-01-20 02:40:58 +0200

    emit received messages through websocket channel, temporary hardcode allowed origin

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,19 +1,27 @@
-import React, {useState} from 'react';
-import {Box, Button, TextField} from "@mui/material";
-import { sendMessage } from "../../api/messagesApi"
+import React, {useEffect, useState} from 'react';
+import {Box, Button, TextField, Typography} from "@mui/material";
+import {sendMessage} from "../../api/messagesApi"
+import {socket} from "../../api/socket";
 
 export const App = () => {
 
     const [message, setMessage] = useState("");
+    const [chatMessages, setChatMessages] = useState<string[]>([]);
+
+    useEffect(() => {
+        socket.on('message', message => setChatMessages(prevState => [...prevState, message]));
+    }, []);
 
     return (
         <Box sx={{
             padding: "20rem"
         }}>
-            <TextField label="Message" variant="outlined" onChange={e => setMessage(e.target.value)} />
+            <TextField label="Message" variant="outlined" onChange={e => setMessage(e.target.value)}/>
             <Button onClick={() => sendMessage(message).then(_ => setMessage(""))}>
                 Send
             </Button>
+
+            {chatMessages.map((m, idx) => <Typography key={idx}>{m}</Typography>)}
         </Box>
     );
 }
